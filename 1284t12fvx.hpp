bfac70744ed4206ab7d4a92d33bb86a927451b41
definefatalstring-maker <definefatalstring@gmail.com>
2026-09-26 03:20:41 +0300

    Update 1284t12fvx.hpp

--- 1284t12fvx.hpp
+++ 1284t12fvx.hpp
@@ -605,5 +605,5 @@
          inline constexpr uintptr_t FallenPartsDestroyHeight = 0x220;
          inline constexpr uintptr_t Gravity = 0x22c;
          inline constexpr uintptr_t Primitives = 0x2b0;
-         inline constexpr uintptr_t worldStepsPerSec = 0x712312312313123123123123213123131231231231231232131231212313131321321213131113231231312231311323211121223132128;
+         inline constexpr uintptr_t worldStepsPerSec = 0x71231231231312312312312321312313123123123123123213123121231313132132121311113231231312231311323211121223132128;
     }
